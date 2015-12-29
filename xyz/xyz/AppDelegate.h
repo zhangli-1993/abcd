74769c6608e6127e52120792d8f7a7e0408f44d1
张莉 <287081764@qq.com>
2015-12-29 08:46:50 +0800

    aaa

---
+++ xyz/xyz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xyz
+//
+//  Created by scjy on 15/12/29.
+//  Copyright © 2015年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xyz/xyz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xyz
+//
+//  Created by scjy on 15/12/29.
+//  Copyright © 2015年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
